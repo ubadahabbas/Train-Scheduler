a9356f1e6bc5350096ad29e4adb08d49fe252025
Ubadah Atta <ubadahatta12@gmail.com>
2019-02-04 21:30:44 -0500

    update

--- app.js
+++ app.js
@@ -1,22 +1,64 @@
-$("#user-page").hide();
-firebase.auth().onAuthStateChanged(function(user) {
+
+$(function () {
+
+    var user = firebase.auth().currentUser;
+$("#show-trains").on("click",function(){
+    
+})
+if (user) {
+    $("#log-in-div").hide();
+    $("#user-page").show();
+} else {
+    $("#log-in-div").show();
+    $("#user-page").hide();
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+    console.log("outside " +user)
     if (user) {
-      $("#log-in-div").hide();
-      $("#user-page").show();
+        $("#log-in-div").hide();
+        $("#user-page").show();
+        console.log("inside " +user)
+        var user = firebase.auth().currentUser;
+        if (user != null){
+            var email_id= user.email;
+        console.log(email_id)
+        }
     } else {
         $("#log-in-div").show();
         $("#user-page").hide();
+        console.log("else " +user)
     }
-  });
+});
 
-$("#log-in").on("submit", function(){
+$("#log-in-button").on("click",function(){
+ 
     var userEmail = $("#userEmail").val().trim();
     var userPassword = $("#userPassword").val().trim();
+console.log(userEmail + userPassword)
 
-    firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
+firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error: "+errorMessage)
+  });
+//     firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function (error) {
+//         // Handle Errors here.
+//         var errorCode = error.code;
+//         var errorMessage = error.message;
+//         // ...
+//         window.alert("Error: "+errorMessage)
+
+// }) 
+   
+})
+
+$("#sign-out").on("click",function(){
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+      }).catch(function(error) {
+        // An error happened.
       });
 })
+});
